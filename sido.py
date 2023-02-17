9c8bfd1a7b813149b2ebe6bf29befce0ec97a4b1
rizredvlo7zer0 <50101349+rizredvlo7zer0@users.noreply.github.com>
2023-02-17 19:24:48 +0700

    Add files via upload

---
+++ sido.py
@@ -0,0 +1,9 @@
+import requests
+
+paket = input("Masukkan nomor paket: ")
+url = "https://sidompul.cloudaccess.host/cek.php?nomor=" + paket
+resp = requests.get(url)
+if resp.status_code == 200:
+    print("Status paket: " + resp.text)
+else:
+    print("Gagal mengambil status paket")
